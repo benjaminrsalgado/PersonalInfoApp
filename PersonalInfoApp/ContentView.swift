631faa0d8e2531da7ddcfc291b97610c05d75b55
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-15 15:36:54 -0600

    Initial Commit

---
+++ PersonalInfoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalInfoApp
+//
+//  Created by Benjamin Rojo on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalInfoApp/PersonalInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalInfoAppApp.swift
+//  PersonalInfoApp
+//
+//  Created by Benjamin Rojo on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
